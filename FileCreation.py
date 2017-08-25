7bf77c7f5a91383413733bd3d8fe3685c2981d02
Phil DeSantis <phildewpoint@gmail.com>
2017-08-25 13:56:53 -0500

    File creation routine updated

--- FileCreation.py
+++ FileCreation.py
@@ -1,12 +1,19 @@
-import datetime, os
+import datetime
+import os
+from appJar import gui
 
-def create_file(api_name=None,):
-    """This function will take """
-    mydir = app.directoryBox(title="myDirectory")
-    if mydir is None:
+
+def create_file(file_name, file_dir):
+    """This function will take a name and directory and return a file object"""
+    if file_name is None or file_dir is None:
+        msg = "The file you're trying to save is missing a directory or name. Please try the program again"
+        app = gui()
+        app.errorBox(title="Missing Key Information", message=msg)
+        app.go()
+        app.stop()
         quit()
     else:
-        file_name = api_name + datetime.date.today().strftime("%d%m%Y")
-        file_path = os.path.join(mydir, file_name)
+        file_name = file_name + datetime.date.today().strftime("%d%m%Y")
+        file_path = os.path.join(file_dir, file_name)
         file = open(file_path, mode='w')
         return file
