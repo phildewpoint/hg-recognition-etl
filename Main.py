6b2ce3d97a7a4d41d7d077598246268ffea4c891
Phil DeSantis <phildewpoint@gmail.com>
2017-08-26 10:43:14 -0500

    Added launching and creation tools

--- Main.py
+++ Main.py
@@ -1,8 +1,35 @@
 from appJar import gui
+from FileCreation import create_file
+init_buttons = [
+    "Continue",
+    "Quit"
+]
+
+
+def launch(button, app):
+    if button == "Quit":
+        quit()
+    # get file location
+    else:
+        file_name = "RecognitionFile"
+        file_dir = None
+        app.infoBox(title="noBox", message="Blue box")
+        print('going')
+        quit()
+    # launch API
 
 
 def create_gui():
     app = gui()
+    msg = "This utility will create two recognition files. \nOne for givers and one for receivers."
+    app.addLabel(title="label_box", text=msg)
+    app.addLabelEntry(title="API Launcher")
+    app.addHorizontalSeparator()
+    app.addButtons(
+        names=init_buttons,
+        funcs=launch
+    )
+    app.go()
 
 
 create_gui()
